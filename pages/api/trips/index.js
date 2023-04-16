65786512893748b59cae8bbd3876561012586f4d
singhcodex <singh.hessen@gmail.com>
2023-04-16 15:44:50 +0200

    after trip and expense get all

--- pages/api/trips/index.js
+++ pages/api/trips/index.js
@@ -4,6 +4,15 @@ export default async function handler(req, res ){
    
     if(req.method === 'GET'){
         const trips = await prisma.trip.findMany()
+        await Promise.all(
+            trips.map(async (trip) => {
+                trip.expenses = await prisma.expense.findMany({
+                    where:{
+                        trip:trip.id,
+                    }
+                })
+            })
+        )
         res.status(200).json(trips)
         return
     }
