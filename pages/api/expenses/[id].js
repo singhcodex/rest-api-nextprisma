1d6f32f0bf8dad0f63a43a85654d45c6051d47d2
singhcodex <singh.hessen@gmail.com>
2023-04-20 10:42:48 +0200

    update and delete trips and expense done

--- pages/api/expenses/[id].js
+++ pages/api/expenses/[id].js
@@ -1,6 +1,7 @@
 import prisma from "lib/prisma";
 
 export default async function handler(req, res){
+
     if(req.method === 'GET'){
         const expense = await prisma.expense.findUnique({
             where: {
@@ -12,10 +13,24 @@ export default async function handler(req, res){
         }
         res.status(200).json(expense)
     }
+
     if(req.method === 'PUT'){
 
-    }
-    if(req.method === 'DELETE'){
+        const {trip, name, date, amount, currency} = req.body
 
+        await prisma.expense.update({
+            data: {trip, name, date, amount, currency},
+            where: {id: parseInt(req.query.id)}
+        })
+
+        return res.status(200).end()
+    }
+
+    if(req.method === 'DELETE'){
+        await prisma.expense.delete({
+            where: {id: parseInt(req.query.id)}
+        })
+
+        res.status(200).end()
     }
 }
